d2b2817242252f57c6474946b79c45cbdf17e37b
eganpg <eganpg@gmail.com>
2014-07-17 16:36:14 -0700

    commit of working version using correct css and angular via mattias

--- angularjs.js
+++ angularjs.js
@@ -1,15 +1,26 @@
 var appName = angular.module('tictactoe', []);
-		appName.controller('Grid', function($scope) {
+appName.controller('Grid', function($scope) {
+
+/*  $scope.traceStyle = "" ;
+
+	$scope.changeColor = function() {
+		console.log('ahhhh!');
+		$scope.traceStyle="{traceStyle='background-color':'blue'}"; 
+		
+	}
+
+
 				counter = 0;
-			$scope.changeColor = function() {
-				alert('ahhh');
-				/*if (counter == 1 || counter == 3 || counter == 5 || counter == 7) {
-					myStyle="{background:'red'}"
+				if (counter == 1) {
+					myStyle === ;
+					console.log(counter, myStyle);
 					counter = counter + 1;
 				}
 				else {
-					myStyle="{background:'black'}"
+					myStyle == 'black';
 					counter = counter + 1;
-				}*/
-			}
-		});
+					console.log(counter, myStyle);
+				} */
+
+
+			});
