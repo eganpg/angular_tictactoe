b847bbf00ac6ce7db83e97952ffd5d9b692090ad
eganpg <eganpg@gmail.com>
2014-07-19 13:09:42 -0700

    working logic for all first column win combinations

--- angularjs.js
+++ angularjs.js
@@ -5,7 +5,6 @@ appName.controller('Grid', function($scope) {
 	$scope.oMoves = 0;
 	$scope.xArray = [];
 	$scope.oArray = [];
-	$scope.threeinarow = 0;
 	$scope.winningCombo = [[1,2,4],[8,16,32],[64,128,256],[1,8,64],[2,16,128],[4,32,256],[1,16,256],[4,16,64]];
 	
 
@@ -20,6 +19,7 @@ appName.controller('Grid', function($scope) {
 			$scope.xArray.push(cell);
 			// console.log($scope.xArray);
 			// console.log(cell);
+			$scope.xArray.sort();
 			$scope.winningArrayX();
 			// $scope.winningNumberX = cell + $scope.winningNumberX;
 			// console.log($scope.winningNumberX); 
@@ -30,7 +30,7 @@ appName.controller('Grid', function($scope) {
 			count = count + 1;
 			$scope.oMoves += cell;
 			$scope.oArray.push(cell);
-			$scope.winningArrayO();
+			// $scope.winningArrayO();
 			// console.log($scope.oArray);
 			// console.log(cell);
 			// $scope.winningNumberY = cell + $scope.winningNumberY;
@@ -46,9 +46,10 @@ $scope.isSet = function(whatever, cell){
 // Call a function 
 $scope.winningArrayX = function() {
 	// don't execute unless there has been more than 4 moves
-	if (count > 4) {
+	// if (count > 4) {
 		//sort the x array to allow for a clean search
-		$scope.xArray.sort();
+		$scope.xArray = $scope.xArray.sort();
+		console.log($scope.xArray.sort())
 		// run a loop though the 8 predefines arrays
 		for (var i = 0; i < $scope.winningCombo.length; i++) {
 			// run a loop that executes a loop based on the number of xArray
@@ -63,41 +64,73 @@ $scope.winningArrayX = function() {
 						else if ($scope.winningCombo[i][j] == $scope.xArray[3]) {
 							alert('XWins');
 						}
+						else if ($scope.winningCombo[i][j] == $scope.xArray[4]) {
+							alert('xWins');
+						}
+						else {
+							return;
+						}
 					}
-				}
-			}
-		}
-	}
-}
-$scope.winningArrayO = function() {
-	if (count > 4) {
-		$scope.xArray.sort();
-		for (var i = 0; i < $scope.winningCombo.length; i++) {
-			for (var j = 0; j < $scope.oArray.length; j++) {
-				if ($scope.winningCombo[i][j] == $scope.oArray[0]) {
-					j++;
-					if ($scope.winningCombo[i][j] == $scope.oArray[1]) {
+					else if ($scope.winningCombo[i][j] == $scope.xArray[2]) {
 						j++;
-						if ($scope.winningCombo[i][j] == $scope.oArray[2]) {
-							alert('O wins');
+						if ($scope.winningCombo[i][j] == $scope.xArray[3]) {
+							alert('xWins');
 						}
-						else if ($scope.winningCombo[i][j] == $scope.oArray[3]) {
-							alert('O Wins');
+						else if ($scope.winningCombo[i][j] == $scope.xArray[4]) {
+							alert('xWins');
+						}
+						else {
+							return;
 						}
 					}
+					else {return;}
 				}
-			}
+				else {return;}
+
+				// else if ($scope.winningCombo[i][j] == $scope.xArray[1]) {
+				// 	j++;
+				// 	console.log($scope.xArray);
+				// 	if ($scope.winningCombo[i][j] == $scope.xArray[2]) {
+				// 		j++;
+				// 		if ($scope.winningCombo[i][j] == $scope.xArray[3]) {
+				// 			alert('x wins');
+				// 		}
+				// 		else if ($scope.winningCombo[i][j] == $scope.xArray[4]) {
+				// 			alert('x wins');
+				// 		}
+				// 	}
+				// }
+			// }
 		}
 	}
 }
+// $scope.winningArrayO = function() {
+// 	if (count > 4) {
+// 		$scope.oArray.sort();
+// 		for (var i = 0; i < $scope.winningCombo.length; i++) {
+// 			for (var j = 0; j < $scope.oArray.length; j++) {
+// 				if ($scope.winningCombo[i][j] == $scope.oArray[0]) {
+// 					j++;
+// 					if ($scope.winningCombo[i][j] == $scope.oArray[1]) {
+// 						j++;
+// 						if ($scope.winningCombo[i][j] == $scope.oArray[2]) {
+// 							alert('O wins');
+// 						}
+// 						else if ($scope.winningCombo[i][j] == $scope.oArray[3]) {
+// 							alert('O Wins');
+// 						}
+// 						else if ($scope.winningCombo[i][j] == $scope.oArray[4]) {
+// 							alert('O wins')
+// 						}
+// 						else {
+// 							$scope.winningArray0();
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 		// }
-		/* Write a Function that logs the value of a specific square */
-
-	// $scope.determineAWinner = function() {
-	// 	console.log($scope.winningNumberX)
-	// 	console.log($scope.winningNumberY)
-	// 	if ( $scope.winningNumberX == 7 || $scope.winningNumberX == 56 || $scope.winningNumberX == 448 || $scope.winningNumberX == 73 || $scope.winningNumberX == 146 || $scope.winningNumberX == 292 || $scope.winningNumberX == 273 || $scope.winningNumberX == 84) {
-	// 		alert('x wins');
-	// 	}
-	// }
+	
 });
