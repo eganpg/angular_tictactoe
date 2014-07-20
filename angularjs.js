4a1031440b837d25061d97833f3bf0526076f864
eganpg <eganpg@gmail.com>
2014-07-19 19:20:22 -0700

    commit of working tic tac toe logic with function x and o pieces. Still super ugly but functioning nevertheless

--- angularjs.js
+++ angularjs.js
@@ -7,7 +7,6 @@ appName.controller('Grid', function($scope) {
 	$scope.oArray = [];
 	$scope.xWinCount = 0;
 	$scope.winningCombo = [[1,2,4],[8,16,32],[64,128,256],[1,8,64],[2,16,128],[4,32,256],[1,16,256],[4,16,64]];
-
 	count = 0;
 	$scope.changeColor = function(cell) {
 		if (count % 2 == 0) {
@@ -25,19 +24,18 @@ appName.controller('Grid', function($scope) {
 			return false;
 		}
 	}
-$scope.sortingFunction = function() {
-	return $scope.xArray.sort();
-}
-
+	$scope.sortingFunction = function() {
+		return $scope.xArray.sort();
+	}
 // Changes the color of the background
 $scope.isSet = function(whatever, cell){
 	return (whatever & cell) == cell;
 }
 // Call a function 
 $scope.winningArrayX = function() {
-		$scope.xArray.sort(function(a,b) {return a-b;})
-		console.log($scope.xArray);
-		for (var i = 0; i < $scope.winningCombo.length; i++)	{
+	$scope.xArray.sort(function(a,b) {return a-b;})
+	console.log($scope.xArray);
+	for (var i = 0; i < $scope.winningCombo.length; i++)	{
 			// for (var j = 0; j < $scope.xArray.length; j++) {
 				if ($scope.winningCombo[i][0] == $scope.xArray[0]) {
 					// j++;
@@ -116,79 +114,88 @@ $scope.winningArrayX = function() {
 					}
 				}
 			}
-	}
-
-				// console.log($scope.winningCombo[i]);
-				// for (var j = 0; j < $scope.winningCombo; j++) {
-					// if (($scope.xArray[1] == $scope.winningCombo[i][0]) || ($scope.xArray[1] == $scope.winningCombo[i][1]) || ($scope.xArray[1] == $scope.winningCombo[i][2]))  {
-						
-					// 	if (($scope.xArray[2] == $scope.winningCombo[i][0]) || ($scope.xArray[2] == $scope.winningCombo[i][1]) || ($scope.xArray[2] == $scope.winningCombo[i][2])) {
-					// 		console.log('you have a winner');
-					// 	}
-					// 	else if (($scope.xArray[3] == $scope.winningCombo[i][0]) || ($scope.xArray[3] == $scope.winningCombo[i][1]) || ($scope.xArray[3] == $scope.winningCombo[i][2])) {
-					// 		console.log('you have a winner');
-					// 	}
-					// 	else if (($scope.xArray[4] == $scope.winningCombo[i][0]) || ($scope.xArray[4] == $scope.winningCombo[i][1]) || ($scope.xArray[4] == $scope.winningCombo[i][2])) {
-					// 		console.log('you have a winner');
-					// 	}
-					// 	else {
-					// 		alert('sometin wong')
-					// 	}
-					// }
-				// }
-				// for(var j = 0; j < $scope.winningCombo.length; j++) {
-				// 	if ($scope.xArray[1] == $scope.winningCombo[i][0])
-				// }
-			
-
-		
-			
-		
-	//sort the x array to allow for a clean search
-		
-	 // $scope.xArray.sort();
-		// console.log($scope.xArray);
-		// // run a loop though the 8 predefines arrays
-		// for (var i = 0; i < $scope.winningCombo.length; i++) {
-		// 	for (var j = 0; j < $scope.xArray.length; j++) {
-		// 		if (($scope.xArray[j] == $scope.winningCombo[i][0]) || ($scope.xArray[j] == $scope.winningCombo[i][1]) || ($scope.xArray[j] == $scope.winningCombo[i][2])) {
-		// 			$scope.xArray.sort();
-		// 			console.log($scope.xArray);
-
-					
-		// 		// $scope.xWinCount++;
-		// 		// console.log($scope.xWinCount)
-		// 		}
-		// 	}
-		// }
-
-// $scope.winningArrayO = function() {
-// 	if (count > 4) {
-// 		$scope.oArray.sort();
-// 		for (var i = 0; i < $scope.winningCombo.length; i++) {
-// 			for (var j = 0; j < $scope.oArray.length; j++) {
-// 				if ($scope.winningCombo[i][j] == $scope.oArray[0]) {
-// 					j++;
-// 					if ($scope.winningCombo[i][j] == $scope.oArray[1]) {
-// 						j++;
-// 						if ($scope.winningCombo[i][j] == $scope.oArray[2]) {
-// 							alert('O wins');
-// 						}
-// 						else if ($scope.winningCombo[i][j] == $scope.oArray[3]) {
-// 							alert('O Wins');
-// 						}
-// 						else if ($scope.winningCombo[i][j] == $scope.oArray[4]) {
-// 							alert('O wins')
-// 						}
-// 						else {
-// 							$scope.winningArray0();
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-		// }
-	
-});
+		}
+$scope.winningArrayO = function() {
+	$scope.oArray.sort(function(a,b) {return a-b;})
+	console.log($scope.oArray);
+	for (var i = 0; i < $scope.winningCombo.length; i++)	{
+			// for (var j = 0; j < $scope.xArray.length; j++) {
+				if ($scope.winningCombo[i][0] == $scope.oArray[0]) {
+					// j++;
+					console.log('made it')
+					if ($scope.winningCombo[i][1] == $scope.oArray[1]) {
+						// j++;
+						if ($scope.winningCombo[i][2] == $scope.oArray[2]) {
+							alert('O wins');
+						}
+						else if ($scope.winningCombo[i][2] == $scope.oArray[3]) {
+							alert('O Wins');
+						}
+						else if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
+							alert('O Wins');
+						}
+						else {
+							return;
+						}
+					}
+					else if ($scope.winningCombo[i][1] == $scope.oArray[1]) {
+						// j++;
+						if ($scope.winningCombo[i][2] == $scope.oArray[3]) {
+							alert('O Wins');
+						}
+						else if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
+							alert('O Wins');
+						}
+						else {
+							return;
+						}
+					}
+					else if ($scope.winningCombo[i][1] == $scope.oArray[3]) {
+						// j++;
+						if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
+							alert('O Wins');
+						}
+						else {
+							return;
+						}
+					}
+				}
+				else if ($scope.winningCombo[i][0] == $scope.oArray[1]) {
+					// j++;
+					if ($scope.winningCombo[i][1] == $scope.oArray[2]) {
+						// j++;
+						if($scope.winningCombo[i][2] == $scope.oArray[3]) {
+							alert('O Wins');
+						}
+						else if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
+							alert('O Wins');
+						}
+						else {
+							return;
+						}
+					}
+					else if ($scope.winningCombo[i][1] == $scope.oArray[3]) {
+						// j++;
+						if($scope.winningCombo[i][2] == $scope.oArray[4]) {
+							alert('O Wins');
+						}
+						else {
+							return;
+						}
+					}
+				}
+				else if ($scope.winningCombo[i][0] == $scope.oArray[2]) {
+					// j++;
+					if ($scope.winningCombo[i][1] == $scope.oArray[3]) {
+						// j++;
+						if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
+							alert('O Wins');
+						}
+						else {
+							return;
+						}
+					}
+				}
+			}
+		}		
+	});
