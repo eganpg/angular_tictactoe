dd9ca34e4b14fa410bf4abaf433263e510a02f28
eganpg <eganpg@gmail.com>
2014-07-21 20:47:15 -0700

    commit of working tic tac toe with some cool angular features including background changes.

--- angularjs.js
+++ angularjs.js
@@ -1,3 +1,7 @@
+
+
+
+
 var appName = angular.module('tictactoe', []);
 appName.controller('Grid', function($scope) {
 	$scope.cells = [[1,2,4],[8,16,32],[64,128,256]]; 
@@ -5,9 +9,27 @@ appName.controller('Grid', function($scope) {
 	$scope.oMoves = 0;
 	$scope.xArray = [];
 	$scope.oArray = [];
-	$scope.xWinCount = 0;
+	$scope.xScore = 0;
+	$scope.oScore = 0;
+	$scope.tieED = 0;
 	$scope.winningCombo = [[1,2,4],[8,16,32],[64,128,256],[1,8,64],[2,16,128],[4,32,256],[1,16,256],[4,16,64]];
 	count = 0;
+	$scope.playerColors = [
+	{color: "url('nyc.jpg')", name: 'NYC'},
+	{color: "url('paris.jpg')", name: 'Paris'},
+	{color: "url('sf.jpg')", name: 'SF'}
+	];
+
+	$scope.reset = function() {
+		for(q = 0; q < 9; q++) {
+			$scope.xArray.pop();
+			$scope.oArray.pop();
+			$scope.xMoves = 0;
+			$scope.oMoves = 0;
+			count = 0;
+			console.log($scope.xArray);
+		}
+	}
 	$scope.changeColor = function(cell) {
 		if (count % 2 == 0) {
 			count = count + 1;	
@@ -31,6 +53,19 @@ appName.controller('Grid', function($scope) {
 $scope.isSet = function(whatever, cell){
 	return (whatever & cell) == cell;
 }
+
+$scope.keepScoreX = function() {
+	$scope.xScore = $scope.xScore + 1;
+	console.log($scope.xScore);
+}
+
+$scope.keepScoreO = function() {
+	$scope.oScore = $scope.oScore + 1;
+	console.log($scope.oScore);
+}
+$scope.tie = function() {
+	$scope.tieED = $scope.tieED + 1;
+}
 // Call a function 
 $scope.winningArrayX = function() {
 	$scope.xArray.sort(function(a,b) {return a-b;})
@@ -43,13 +78,16 @@ $scope.winningArrayX = function() {
 					if ($scope.winningCombo[i][1] == $scope.xArray[1]) {
 						// j++;
 						if ($scope.winningCombo[i][2] == $scope.xArray[2]) {
-							alert('Xwins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.xArray[3]) {
-							alert('XWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.xArray[4]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -58,10 +96,12 @@ $scope.winningArrayX = function() {
 					else if ($scope.winningCombo[i][1] == $scope.xArray[2]) {
 						// j++;
 						if ($scope.winningCombo[i][2] == $scope.xArray[3]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.xArray[4]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -70,7 +110,8 @@ $scope.winningArrayX = function() {
 					else if ($scope.winningCombo[i][1] == $scope.xArray[3]) {
 						// j++;
 						if ($scope.winningCombo[i][2] == $scope.xArray[4]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -82,10 +123,12 @@ $scope.winningArrayX = function() {
 					if ($scope.winningCombo[i][1] == $scope.xArray[2]) {
 						// j++;
 						if($scope.winningCombo[i][2] == $scope.xArray[3]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.xArray[4]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -94,7 +137,8 @@ $scope.winningArrayX = function() {
 					else if ($scope.winningCombo[i][1] == $scope.xArray[3]) {
 						// j++;
 						if($scope.winningCombo[i][2] == $scope.xArray[4]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -106,7 +150,8 @@ $scope.winningArrayX = function() {
 					if ($scope.winningCombo[i][1] == $scope.xArray[3]) {
 						// j++;
 						if ($scope.winningCombo[i][2] == $scope.xArray[4]) {
-							alert('xWins');
+							$scope.keepScoreX(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -126,13 +171,16 @@ $scope.winningArrayO = function() {
 					if ($scope.winningCombo[i][1] == $scope.oArray[1]) {
 						// j++;
 						if ($scope.winningCombo[i][2] == $scope.oArray[2]) {
-							alert('O wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.oArray[3]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -141,10 +189,12 @@ $scope.winningArrayO = function() {
 					else if ($scope.winningCombo[i][1] == $scope.oArray[2]) {
 						// j++;
 						if ($scope.winningCombo[i][2] == $scope.oArray[3]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -153,7 +203,8 @@ $scope.winningArrayO = function() {
 					else if ($scope.winningCombo[i][1] == $scope.oArray[3]) {
 						// j++;
 						if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -165,10 +216,12 @@ $scope.winningArrayO = function() {
 					if ($scope.winningCombo[i][1] == $scope.oArray[2]) {
 						// j++;
 						if($scope.winningCombo[i][2] == $scope.oArray[3]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else if ($scope.winningCombo[i][2] == $scope.oArray[4]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else {
 							return;
@@ -177,7 +230,8 @@ $scope.winningArrayO = function() {
 					else if ($scope.winningCombo[i][1] == $scope.oArray[3]) {
 						// j++;
 						if($scope.winningCombo[i][2] == $scope.oArray[4]) {
-							alert('O Wins');
+							$scope.keepScoreO(); 
+							$scope.reset();
 						}
 						else {
 							return;
